a53db5e97ac63bc1a1c7c3edc679267263896c0d
agmmnn <agmyak66@gmail.com>
2021-10-03 17:30:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, find_packages
-import nisanyan_cli
+from setuptools import setup
+from nisanyan_cli.__main__ import __version__ as version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,22 +7,20 @@ with open("README.md", "r") as fh:
 with open("requirements.txt", "r") as file:
     requires = [line.strip() for line in file.readlines()]
 
-VERSION = "0.0.1"
-DESCRIPTION = "CLI for nisanyansozluk.com (nis <word>)"
+DESCRIPTION = (
+    "CLI tool for Turkish etymological dictionary, nisanyansozluk.com. (nis <word>)"
+)
 
 setup(
-    name="nisanyan_cli",
-    version=VERSION,
+    name="nisanyan-cli",
+    version=version,
     url="https://github.com/agmmnn/nisanyan-cli",
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="agmmnn",
-    license="Apache License Version 2.0",
-    license_files=["LICENSE"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Environment :: Console",
         "Topic :: Utilities",
